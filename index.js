fb84b3a88e7453db670e13da848b3f8cb3ccd20d
Al Mohaimin Farabi <almohaiminfarabi36@gmail.com>
2021-10-27 00:54:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@genouscarmechanisc.imf7s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = await client.db("carMechanic");
+    const servicesCollection = await database.collection("services");
+
+    //  POST Api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log("hit the post api");
+      const result = await servicesCollection.insertOne(service);
+      //   console.log(result);
+      res.send(result);
+    });
+
+    //   Delete Api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //   GET Api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET Single Service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
+});
+
+app.listen(port, () => {
+  console.log("Running genius server on port ", port);
+});
